ce319dd7325448b8f4845f0bcf5050aff9131980
hicham bouzid <bouzidhicham51@gmail.com>
2025-02-26 00:50:01 +0000

    PUSH-0.0.0: the first two exercises seems easy .

---
+++ Python-00/ex00/Hello.py
@@ -0,0 +1,21 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello" , "tutu!"}
+ft_dict = {"Hello" : "1337!"}
+#your code here
+ft_list[1] = "World"
+
+ft_tuple = list(ft_tuple)
+ft_tuple[1] = "Morocco"
+ft_tuple = tuple(ft_tuple)
+
+# ft_set.remove("Hello")
+# ft_set.remove("tutu!")
+#because set print element Unorderd
+ft_set = "{'hello', 'Ben gruir!'}"
+# ft_set.add(Hello ', 'Ben gruir)
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
---
+++ Python-00/ex01/format_ft_time.py
@@ -0,0 +1,12 @@
+import time
+
+
+print("Seconds since January 1, 1970: " , "{:,.4f}".format(time.time()), " or ", '{:.2e}'.format(time.time()), " in scientific notation")
+# print(f"{Stime:.d}")
+
+
+
+
+num = 1000000000
+
+# print("{:,f}".format(num))
